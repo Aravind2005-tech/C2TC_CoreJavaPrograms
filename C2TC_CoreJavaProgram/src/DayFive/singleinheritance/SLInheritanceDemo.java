50a50e2bf55ee6cca249859dac792e3bc3cb6a12
Aravind2005-tech <aravindmvit03@gmail.com>
2025-07-29 16:24:50 +0530

    pushedd

---
+++ C2TC_CoreJavaProgram/src/DayFive/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,12 @@
+package DayFive.singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+
+		Student student = new Student("Abineshwar", "8080808080", "Pondicherry", 79048788497L, 10, "City College");
+		System.out.println(student);
+
+	}
+
+}
