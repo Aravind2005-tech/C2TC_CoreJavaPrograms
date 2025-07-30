df82b8fade2ac0c5aefae97a4956728c55b6c29c
Aravind2005-tech <aravindmvit03@gmail.com>
2025-07-30 13:31:04 +0530

    hey

--- C2TC_CoreJavaProgram/src/day_Eight/abstarction/Square.java
+++ C2TC_CoreJavaProgram/src/day_Eight/abstarction/Square.java
@@ -3,7 +3,7 @@ package day_Eight.abstarction;
 public class Square {
 
     public void calArea() {
-        // TODO Auto-generated method stub
+        
         throw new UnsupportedOperationException("Unimplemented method 'calArea'");
     }
 
